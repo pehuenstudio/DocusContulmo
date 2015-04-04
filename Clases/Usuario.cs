1da064261e282dc11112408922d34e73d7ea76dd
Rodrigo Sepùlveda Castro <rodrigo.secastro@gmail.com>
2015-04-04 00:04:02 -0300

    v0.05

--- Clases/Usuario.cs
+++ Clases/Usuario.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DocusContulmo.Clases
 {
     class Usuario
     {
+        private int IdTipoDeUsuario;
+        private string RUN;
+        private string Nombre1;
+        private string Nombre2;
+        private string Apellido1;
+        private string Apellido2;
+        private string Cargo;
+        private string Email;
+        private string Pass;
+        private string UltimaVisita;
+        private int Estado;
+
+
+        public Usuario(){}
+
+        public Usuario(string RUN, string Pass)
+        {
+            this.RUN = RUN;
+            this.Pass = Pass;
+        }
+
+        public Boolean ValidarUsuario(){
+            
+            return true;
+        }
         
     }
 }
